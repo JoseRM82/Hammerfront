fa751e8ea65dfeaff2e6651e87f7c4beb938e879
Jose <josermatosg@outlook.com>
2024-04-08 11:23:06 -0300

    Config Update

--- next.config.js
+++ next.config.js
@@ -1,16 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   compiler: {
+//     styledComponents: true,
+//   },
+// }
+
+
+module.exports = {
   reactStrictMode: true,
-  swcMinify: true,
   compiler: {
     styledComponents: true,
   },
-}
-
-module.exports = nextConfig
-
-module.exports = {
   env: {
-    BASE_URL: 'https://hammer-backend.onrender.com/api/',
+    NEXT_PUBLIC_BASE_URL: process.env.BASE_URL,
+    NEXT_PUBLIC_TEST_ID: process.env.TEST_ID,
+    NEXT_PUBLIC_TEST_TYPE: process.env.TEST_TYPE,
+    NEXT_PUBLIC_TEST_NAME: process.env.TEST_NAME,
+    NEXT_PUBLIC_TEST_TOKEN: process.env.TEST_TOKEN,
+    NEXT_PUBLIC_TEST_PASS: process.env.TEST_PASS,
   }
 }
+
+// module.exports = nextConfig
