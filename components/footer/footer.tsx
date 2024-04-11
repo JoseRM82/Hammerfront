5f0bb218a298bdad2fee88e9fdca1b8fcd67968a
Jose <josermatosg@outlook.com>
2024-04-10 21:53:04 -0300

    Footer Update

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -4,7 +4,7 @@ import { FunctionComponent } from "react";
 
 import facebook from './facebook.svg';
 import twitter from './twitter.svg';
-import instagram from './instagram.svg';
+import github from './github.svg';
 import linkedin from './linkedin.svg';
 
 const Footer: FunctionComponent<Props> = ({ className }) => {
@@ -15,10 +15,10 @@ const Footer: FunctionComponent<Props> = ({ className }) => {
           Jose Matos in association with Hammer & co.
         </div>
         <div className="footer-social">
-          <Link href='https://www.facebook.com'><a target={"_blank"}><div className="social-link" ><Image src={facebook} height={30} width={30} /></div></a></Link>
-          <Link href='https://www.twitter.com'><a target={"_blank"}><div className="social-link" ><Image src={twitter} height={30} width={30} /></div></a></Link>
-          <Link href='https://web.whatsapp.com'><a target={"_blank"}><div className="social-link" ><Image src={instagram} height={30} width={30} /></div></a></Link>
-          <Link href='https://www.linkedin.com'><a target={"_blank"}><div className="social-link" ><Image src={linkedin} height={30} width={30} /></div></a></Link>
+          {/* <Link href='https://www.facebook.com'><a target={"_blank"}><div className="social-link" ><Image src={facebook} height={30} width={30} /></div></a></Link>
+          <Link href='https://www.twitter.com'><a target={"_blank"}><div className="social-link" ><Image src={twitter} height={30} width={30} /></div></a></Link> */}
+          <Link href='https://github.com/JoseRM82/Hammerfront'><a target={"_blank"}><div className="social-link" ><Image src={github} height={30} width={30} /></div></a></Link>
+          <Link href='https://www.linkedin.com/in/jos%C3%A9-matos-gonzales-8460572ba/'><a target={"_blank"}><div className="social-link" ><Image src={linkedin} height={30} width={30} /></div></a></Link>
         </div>
       </div>
     </div>
